d0effd6782962029051f56818753b60fbe232904
harshal <harshal.c@amazatic.net>
2020-04-08 13:33:04 +0530

    Added directory creation in serve.py

--- src/serve.py
+++ src/serve.py
@@ -10,6 +10,8 @@ from core.wsgi import APPLICATION
 BASE_DIR = os.path.join(os.path.dirname(__file__), 'src')
 sys.path.append(BASE_DIR)
 
+os.system('mkdir /tmp/.pyzoho')
+os.system('touch /tmp/.pyzoho/zcrm_oauthtokens.pkl')
 waitress.serve(
     APPLICATION,
     host='0.0.0.0',
