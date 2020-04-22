fae95fe60b20feb8accbe28eb8ffa9523ca709d7
vikrant <vikrant.g@amazatic.com>
2020-04-22 18:13:18 +0530

    Custom user model admin added & only approve user displayed.

--- src/user/admin.py
+++ src/user/admin.py
@@ -5,9 +5,37 @@ from django import forms
 from django.contrib import admin
 from core.mailer import mail
 from django.conf import settings
+from datetime import datetime
 from .models import (User, MemberCategory,)
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 
 
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+        
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    list_display = ('email', 'is_approved', )
+    list_filter = ('is_approved', 'is_verified')
+    list_per_page = 25
+    search_fields = ('username', 'legal_business_name', 'email',)
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('is_approved',)}),
+    )
+   
+
+
+    def save_model(self, request, obj, form, change):
+        if obj.is_approved:
+            mail("approved.html",{'link': settings.FRONTEND_DOMAIN_NAME},"Account Approved.", obj.email)
+        super().save_model(request, obj, form, change)
+
+    
+        
 # class UserForm(forms.ModelForm):
 #     manual_activation = forms.BooleanField()  # a flag which determines if the user should be manually activated
 
@@ -26,22 +54,22 @@ from .models import (User, MemberCategory,)
 
 #         return self.cleaned_data
 
-class UserAdmin(admin.ModelAdmin):
-    """
-    Configuring User
-    """
-    #form = UserForm
-    list_per_page = 50
-    readonly_fields = ['zoho_contact_id']
-    search_fields = ('username', 'legal_business_name', 'email',)
-    list_display = ('email', 'is_approved','is_verified', )
-    list_filter = ('is_approved', 'is_verified')
+# class UserAdmin(admin.ModelAdmin):
+#     """
+#     Configuring User
+#     """
+#     #form = UserForm
+#     list_per_page = 50
+#     readonly_fields = ['zoho_contact_id']
+#     search_fields = ('username', 'legal_business_name', 'email',)
+#     list_display = ('email', 'is_approved','is_verified', )
+#     list_filter = ('is_approved', 'is_verified')
 
 
-    def save_model(self, request, obj, form, change):
-        if obj.is_approved:
-            mail("approved.html",{'link': settings.FRONTEND_DOMAIN_NAME},"Account Approved.", obj.email)
-        super().save_model(request, obj, form, change)
+#     def save_model(self, request, obj, form, change):
+#         if obj.is_approved:
+#             mail("approved.html",{'link': settings.FRONTEND_DOMAIN_NAME},"Account Approved.", obj.email)
+#         super().save_model(request, obj, form, change)
 
 class MemberCategoryAdmin(admin.ModelAdmin):
     """
@@ -49,8 +77,7 @@ class MemberCategoryAdmin(admin.ModelAdmin):
     """
     #search_fields = ('',)
     
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User, MyUserAdmin)
 admin.site.register(MemberCategory, MemberCategoryAdmin)  
 
 
