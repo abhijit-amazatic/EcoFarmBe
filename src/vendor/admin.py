ec5dce68b079dab0afcb3344db605a94cf1dec73
vikrant <vikrant.g@amazatic.com>
2020-04-30 18:00:39 +0530

    Vendor& vendoruser admin added.

--- src/vendor/admin.py
+++ src/vendor/admin.py
@@ -1,31 +1,41 @@
 """
 Admin related customization.
 """
-# from django.contrib import admin
-# from .models import (Vendor,VendorProfile,)
+from django.contrib import admin
+from .models import (Vendor,VendorProfile,VendorUser,)
 
 
-# class InlineVendorProfileAdmin(admin.TabularInline):
-#     """
-#     Configuring field admin view for VendorProfile model
-#     """
-#     extra = 0
-#     model = VendorProfile
-#     readonly_fields = ['vendor', 'profile_type','is_updated_in_crm','zoho_crm_id', 'is_draft', 'step']
+class VendorUserInline(admin.TabularInline):
+    extra = 0
+    model = VendorUser
+    fields = ('user', 'role', )
+    raw_id_fields = ('user', )
+    autocomplete_lookup_fields = {
+        'fk': ['user', ],
+    }
+    
+class InlineVendorProfileAdmin(admin.TabularInline):
+    """
+    Configuring field admin view for VendorProfile model
+    """
+    extra = 0
+    model = VendorProfile
+    readonly_fields = ['vendor', 'profile_type','is_updated_in_crm','zoho_crm_id', 'is_draft', 'step', 'status']
 
     
-# class MyVendorAdmin(admin.ModelAdmin):
-#     """
-#     Configuring Vendors
-#     """
-#     inlines = (InlineVendorProfileAdmin, )
-#     extra = 0
-#     model = Vendor
-#     readonly_fields = ['ac_manager','vendor_category']
-#     list_display = ('vendor_category','ac_manager')
-#     #list_per_page = 50
-#     #search_fields = ('ac_manager__email',)
+class MyVendorAdmin(admin.ModelAdmin):
+    """
+    Configuring Vendors
+    """
+    inlines = (InlineVendorProfileAdmin, VendorUserInline)
+    extra = 0
+    model = Vendor
+    readonly_fields = ['ac_manager','vendor_category']
+    list_display = ('vendor_category','ac_manager')
+    search_fields = ('ac_manager__email','vendor_category')
+    #list_per_page = 50
+    #search_fields = ('ac_manager__email',)
 
 
 
-# admin.site.register(Vendor,MyVendorAdmin)
+admin.site.register(Vendor,MyVendorAdmin)
