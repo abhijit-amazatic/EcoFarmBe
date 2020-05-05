d4f63dba61e42eb033ac89494cbe6190d2465a07
vikrant <vikrant.g@amazatic.com>
2020-05-05 17:24:21 +0530

    Inventory filters added for multiple fields and multiple values.

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -7,17 +7,24 @@ from rest_framework import (viewsets, status,)
 from rest_framework.filters import (OrderingFilter, )
 from rest_framework.permissions import (IsAuthenticated, AllowAny)
 from django_filters import rest_framework as filters
+from django_filters import (BaseInFilter, CharFilter, FilterSet)
 from .serializers import (InventorySerialier, LogoutInventorySerializer, )
 from .models import (Inventory, )
 
-class InventoryViewSet(viewsets.ModelViewSet):
-    """
-    Inventory View
-    """
-    permission_classes = (AllowAny, )
-    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
-    filterset_fields = {
-        'name':['icontains', 'exact'],
+class CharInFilter(BaseInFilter,CharFilter):
+    pass
+
+class DataFilter(FilterSet):   
+    name__in = CharInFilter(field_name='name', lookup_expr='in')
+    product_type__in = CharInFilter(field_name='product_type', lookup_expr='in')
+    cf_cultivar_type__in = CharInFilter(field_name='cf_cultivar_type', lookup_expr='in')
+    cf_cannabis_grade_and_category__in = CharInFilter(field_name='cf_cannabis_grade_and_category', lookup_expr='in')
+    cf_cbd__in = CharInFilter(field_name='cf_cbd', lookup_expr='in')
+    cf_potency__in = CharInFilter(field_name='cf_potency', lookup_expr='in')
+    
+    class Meta:
+        model = Inventory
+        fields = {
         'sku':['icontains', 'exact'],
         'category_name':['icontains', 'exact'],
         'cf_cultivar_type':['icontains', 'exact'],
@@ -28,8 +35,18 @@ class InventoryViewSet(viewsets.ModelViewSet):
         'available_stock':['gte', 'lte', 'gt', 'lt'],
         'stock_on_hand':['gte', 'lte', 'gt', 'lt'],
         'cf_cannabis_grade_and_category':['icontains', 'exact'],
-        'last_modified_time':['gte', 'lte', 'gt', 'lt']
+        'last_modified_time':['gte', 'lte', 'gt', 'lt'],
+        'product_type':['icontains', 'exact']
         }
+
+
+class InventoryViewSet(viewsets.ModelViewSet):
+    """
+    Inventory View
+    """
+    permission_classes = (AllowAny, )
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filterset_class = DataFilter
     ordering_fields = '__all__'
     
     def get_serializer_class(self):
