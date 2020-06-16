1fdbf16dd629534d1c3646210c344ef87e1a48e1
harshal <harshal.c@amazatic.net>
2020-06-16 23:17:11 +0530

    Licenses fields modified

--- src/user/views.py
+++ src/user/views.py
@@ -54,11 +54,11 @@ class SearchQueryView(APIView):
 
     def get(self, request):
         if request.query_params.get('legal_business_name', None):
-            result = search_query('Licenses', request.query_params['legal_business_name'], 'Name')
+            result = search_query('Licenses', request.query_params['legal_business_name'], 'Name', True)
         elif request.query_params.get('business_dba', None):
-            result = search_query('Licenses', request.query_params['business_dba'], 'Business_DBA')
+            result = search_query('Licenses', request.query_params['business_dba'], 'Business_DBA', True)
         elif request.query_params.get('license_number', None):
-            result = search_query('Licenses', request.query_params['license_number'], 'Legal_Business_Name')
+            result = search_query('Licenses', request.query_params['license_number'], 'Legal_Business_Name', True)
         return Response(result)
 
 class UserViewSet(ModelViewSet):
