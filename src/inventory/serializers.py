c9c59d5885cff6e163f61d6a27d8f02a75ffaf2f
harshal <harshal.c@amazatic.net>
2020-07-10 11:38:47 +0530

    - Implemented an API to create estimate in Zoho books. - Removed Additional fields from inventory models. - Modified inventory code to support above condition.

--- src/inventory/serializers.py
+++ src/inventory/serializers.py
@@ -39,8 +39,6 @@ class LogoutInventorySerializer(serializers.ModelSerializer):
         fields = (
             'cf_strain_name',
             'cf_cultivation_type',
-            'cf_potency',
-            'cf_cbd',
             'cf_cannabis_grade_and_category',
             'available_stock',
             'category_name')
