c802f64e690bacef0ad165bca30df12f327d5879
harshal <harshal.c@amazatic.net>
2020-07-15 15:49:02 +0530

    Inventory bug fixed

--- src/integration/tasks.py
+++ src/integration/tasks.py
@@ -10,13 +10,15 @@ from inventory.models import (Inventory, )
 from .crm import (insert_users, insert_vendors)
 from .inventory import (fetch_inventory, )
 
-def fetch(inventory_name):
+
+@periodic_task(run_every=(crontab(hour=[9], minute=0)), options={'queue': 'general'})
+def fetch_inventory_efd_on_interval():
     """
-    Wrapper defination to fetch inventory.
+    Update inventory on every interval from Zoho Inventory.
     """
     try:
         inventory_before = Inventory.objects.all().delete()
-        fetch_inventory(inventory_name, days=150)
+        fetch_inventory('inventory_efd', days=150)
         inventory_after = Inventory.objects.all().count()
         return {'status_code': 200,
                 'deleted': inventory_before[0],
@@ -25,16 +27,16 @@ def fetch(inventory_name):
         return {'status_code': 400,
                 'error': exc}
 
-@periodic_task(run_every=(crontab(hour=[9], minute=0)), options={'queue': 'general'})
-def fetch_inventory_efd_on_interval():
-    """
-    Update inventory on every interval from Zoho Inventory.
-    """
-    return fetch('inventory_efd')
-
 @periodic_task(run_every=(crontab(hour=[10], minute=0)), options={'queue': 'general'})
 def fetch_inventory_efl_on_interval():
     """
     Update inventory on every interval from Zoho Inventory.
     """
-    return fetch('inventory_efl')
+    try:
+        fetch_inventory('inventory_efl', days=150)
+        inventory_after = Inventory.objects.all().count()
+        return {'status_code': 200,
+                'inserted': inventory_after}
+    except Exception as exc:
+        return {'status_code': 400,
+                'error': exc}
