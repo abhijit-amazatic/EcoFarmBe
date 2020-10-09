24b25faede88ba3af535cb3f30a0a1b240118aaf
harshal <harshal.c@amazatic.net>
2020-10-09 19:13:21 +0530

    Modified code for pre-signed url

--- src/integration/apps/aws.py
+++ src/integration/apps/aws.py
@@ -27,28 +27,19 @@ def get_boto_client(resource,
 def create_presigned_post(bucket_name, object_name, expiration=3600):
     """
     Generate a presigned URL S3 POST request to upload a file
-
-    @param bucket_name: string
-    @param object_name: string
-    @param fields: Dictionary of prefilled form fields
-    @param conditions: List of conditions to include in the policy
-    @param expiration: Time in seconds for the presigned URL to remain valid
-    @return: Dictionary with the following keys:
-        url: URL to post to
-        fields: Dictionary of form fields and values to submit with the POST
-    :return: None if error.
     """
-    config = Config(s3={'addressing_style': 'path'}, signature_version='s3v4')
-    s3_client = get_boto_client('s3', config=config)
+    s3_client = get_boto_client('s3')
     try:
-        response = s3_client.generate_presigned_post(
-            bucket_name,
-            object_name,
-            ExpiresIn=expiration)
+        response = s3_client.generate_presigned_url('put_object',
+                                             Params={
+                                                 'Bucket': bucket_name,
+                                                 'Key': object_name,
+                                                 'ACL': 'private'},
+                                             ExpiresIn=expiration)
     except ClientError as exc:
         return {'error': f'Error while creating presigned url - {exc}',
                 'status_code': 1}
-    return {'status_code': 0, 'response': response}
+    return {'status_code': 0, 'url': response}
 
 def create_presigned_url(bucket_name, object_name, expiration=3600):
     """
