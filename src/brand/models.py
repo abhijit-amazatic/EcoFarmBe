aef702cdda374ee29c67eb9e1071c78b7c149276
vikrant <vikrant.g@amazatic.com>
2020-10-19 17:34:03 +0530

    product_category added in brand.

--- src/brand/models.py
+++ src/brand/models.py
@@ -18,6 +18,7 @@ class Brand(TimeStampFlagModelMixin,models.Model):
     ac_manager = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('Account Manager'),
                                    related_name='manages', null=True, blank=True, default=None, on_delete=models.CASCADE)
     brand_category = ArrayField(models.CharField(max_length=255, blank=True),blank=True, null=True, default=list)
+    product_category = ArrayField(models.CharField(max_length=255, blank=True),blank=True, null=True, default=list)
     brand_county = ArrayField(models.CharField(max_length=255, blank=True),blank=True, null=True, default=list)
     region = models.CharField(_('Region'), blank=True, null=True, max_length=255)
     appellation = models.CharField(_('Brand Appellation'), blank=True, null=True, max_length=255)
