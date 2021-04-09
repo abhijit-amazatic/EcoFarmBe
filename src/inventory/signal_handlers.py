bb55747134abcf67eb1a4ee1f8d2941c8eb36e90
manish.w <manish.w@amazatic.com>
2021-04-09 10:57:41 +0530

    inventory history reson format changed

--- src/inventory/signal_handlers.py
+++ src/inventory/signal_handlers.py
@@ -37,9 +37,7 @@ def pre_save_inventory(sender, instance, **kwargs):
                 for k, v in cd.items()
                 if not k.startswith('_') and k not in ('created_time', 'last_modified_time') and (od[k] or v) and getattr(old_instance, k, '') != getattr(instance, k, '')
             }
-            diff_msg = ''
-            for k, v in diff.items():
-                diff_msg += f"{k}: {v[0]} to {v[1]}  | "
+            diff_msg = '  |  '.join([f"{k}: {v[0]} to {v[1]}" for k, v in diff.items()])
             instance._change_reason = diff_msg
             if diff_msg:
                 if hasattr(instance, 'skip_history_when_saving'):
