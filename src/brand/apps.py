43e617b37cf3bae98b31d2856656308f4764bc3a
vikrant <vikrant.g@amazatic.com>
2021-06-21 18:15:44 +0530

    Updated Verbose for brand module I2235.

--- src/brand/apps.py
+++ src/brand/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class BrandConfig(AppConfig):
     name = 'brand'
+    verbose_name = "Organization"
 
     def ready(self):
         from . import signal_handlers
