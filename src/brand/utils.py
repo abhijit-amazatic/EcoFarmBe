42b4f22eb18235b75d9331e413115d794974f768
manish <manish.w@amazatic.com>
2021-07-14 21:08:57 +0530

    Added method to fetch account and vendor owner.

--- src/brand/utils.py
+++ src/brand/utils.py
@@ -1,7 +1,9 @@
-from django.apps import apps
 import random
-from brand.permission_defaults import DEFAULT_ROLE_PERM
+from django.apps import apps
 
+from integration.crm import get_crm_obj
+from brand.permission_defaults import DEFAULT_ROLE_PERM
+from .models import LicenseProfile
 
 def get_unique_org_name(organization_model):
     name = f'My Organization {int(random.random()*100000):05}'
@@ -20,3 +22,64 @@ def add_default_organization_role():
             )
             role.permissions.set(perms_ls)
             role.save()
+
+def fetch_account_owners(qs=None, offset=0):
+    if not qs:
+        qs = LicenseProfile.objects.all()
+        qs.filter(zoho_crm_account_id='').update(
+            crm_account_owner_id='',
+            crm_account_owner_email='',
+        )
+        qs.filter(zoho_crm_account_id__isnull=True).update(
+            crm_account_owner_id='',
+            crm_account_owner_email='',
+        )
+
+    account_ids = ','.join([id for id in qs.values_list('zoho_crm_account_id', flat=True) if id])
+    crm_obj = get_crm_obj()
+    query = (
+        f"SELECT id,Owner.email,Owner.id FROM Accounts WHERE id in ({account_ids}) LIMIT 200 OFFSET {offset}"
+    )
+    resp = crm_obj.get_coql_query(query=query)
+    if resp is not None:
+        if resp.get('status_code') == 200:
+            data = resp.get('data', [])
+            for account in data:
+                qs.filter(zoho_crm_account_id=account.get('id')).update(
+                    crm_account_owner_id=account.get('Owner.id'),
+                    crm_account_owner_email=account.get('Owner.email'),
+                )
+            if resp.get('info', {}).get('more_records'):
+                fetch_account_owners(qs=qs, offset=offset+200)
+        else:
+            print(resp)
+
+def fetch_vendor_owners(qs=None, offset=0):
+    if not qs:
+        qs = LicenseProfile.objects.all()
+        qs.filter(zoho_crm_vendor_id__isnull='').update(
+            crm_vendor_owner_id='',
+            crm_vendor_owner_email='',
+        )
+        qs.filter(zoho_crm_vendor_id__isnull=True).update(
+            crm_vendor_owner_id='',
+            crm_vendor_owner_email='',
+        )
+    vendor_ids = ','.join([id for id in qs.values_list('zoho_crm_vendor_id', flat=True) if id])
+    crm_obj = get_crm_obj()
+    query = (
+        f"SELECT id,Owner.email,Owner.id FROM Vendors WHERE id in ({vendor_ids}) LIMIT 200 OFFSET {offset}"
+    )
+    resp = crm_obj.get_coql_query(query=query)
+    if resp is not None:
+        if resp.get('status_code') == 200:
+            data = resp.get('data', [])
+            for vendor in data:
+                qs.filter(zoho_crm_vendor_id=vendor.get('id')).update(
+                    crm_vendor_owner_id=vendor.get('Owner.id'),
+                    crm_vendor_owner_email=vendor.get('Owner.email'),
+                )
+            if resp.get('info', {}).get('more_records'):
+                fetch_vendor_owners(qs=qs, offset=offset+200)
+        else:
+            print(resp)
