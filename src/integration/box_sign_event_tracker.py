a825d008664a5573793cac03c5097db6fc55a38a
manish <manish.w@amazatic.com>
2022-01-13 21:05:54 +0530

    Updated box sign event tracker thred

--- src/integration/box_sign_event_tracker.py
+++ src/integration/box_sign_event_tracker.py
@@ -12,7 +12,7 @@ from .models import (
 TRACKER_THREAD_NAME = 'box_sign_event_tracker'
 
 class BoxSignEventTracker(threading.Thread):
-
+    BOX_SIGN_EVENT_TRACKING_INTERVAL = 2
     event_types = [
         # 'SIGN_DOCUMENT_ASSIGNED',
         'SIGN_DOCUMENT_CANCELLED',
@@ -29,18 +29,15 @@ class BoxSignEventTracker(threading.Thread):
 
     def __init__(self):
         super().__init__()
-        self.BOX_SIGN_EVENT_TRACKING_INTERVAL = 2
         if hasattr(settings, 'BOX_SIGN_EVENT_TRACKING_INTERVAL'):
             self.BOX_SIGN_EVENT_TRACKING_INTERVAL = settings.BOX_SIGN_EVENT_TRACKING_INTERVAL
             if self.BOX_SIGN_EVENT_TRACKING_INTERVAL < 1:
-                raise Exception("""
-                DRF API LOGGER EXCEPTION
-                Value of DRF_LOGGER_INTERVAL must be greater than 0
-                """)
+                raise Exception("""Value of BOX_SIGN_EVENT_TRACKING_INTERVAL must be greater than 0.""")
         self.client = get_box_client()
         self.model_obj, _ = BoxEventTracker.objects.get_or_create(id='box_sign')
 
     def run(self):
+        print("******* Box Sign Event Tracker Started.")
         while True:
             time.sleep(self.BOX_SIGN_EVENT_TRACKING_INTERVAL)
             try:
@@ -49,26 +46,24 @@ class BoxSignEventTracker(threading.Thread):
                 print(e)
 
     def process_events(self):
-        print("****************")
         events = self.client.events()
         self.model_obj.refresh_from_db()
         if self.model_obj.is_state_ideal:
-            self.model_obj.is_state_ideal = True
+            self.model_obj.is_state_ideal = False
             self.model_obj.save()
             events_stream = events.get_admin_events_streaming(
                 event_types=self.event_types,
                 stream_position=self.model_obj.stream_position
             )
+            self.model_obj.stream_position = events_stream['next_stream_position']
+            self.model_obj.is_state_ideal = True
+            self.model_obj.save()
             file_id_ls = []
             for event in events_stream['entries']:
                 files = event.additional_details.get('sign_request', {}).get('files')
                 print(f'Event {event.event_type}: {files} at {event.created_at}')
 
                 file_id_ls += [f.id for f  in files]
-
-            self.model_obj.stream_position = events_stream['next_stream_position']
-            self.model_obj.is_state_ideal = True
-            self.model_obj.save()
 
             if file_id_ls:
                 for obj in BoxSign.objects.filter(output_file_id__in=file_id_ls).distinct():
